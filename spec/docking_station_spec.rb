bb701500fab2b0e4027c07d7c765ad5a782f6b7f
Student <Student@MA123.home>
2021-08-26 20:35:00 +0100

    chapter 14 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,38 +1,55 @@
 require "docking_station"
 
 describe DockingStation do
-  it "DockingStation object to respond to .release_bike" do
-    expect(subject).to respond_to(:release_bike)
-    p subject
-    # it { is_expected.to respond_to :release_bike }
+  describe '#release_bike' do
+    it "DockingStation object to respond to #release_bike" do
+      expect(subject).to respond_to(:release_bike)
+      # it { is_expected.to respond_to :release_bike }
+    end
+
+    it "get a bike from .release_bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq(bike)
+    end
+
+    it "expects a bike to be working" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike.working?).to eq true
+      # expect(Bike.new).to be_working
+    end
+
+    it "raises an error when there are no bikes available" do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
-  it "get a bike from .release_bike" do
-    expect(subject.release_bike).to be_a Bike
+  describe '#dock' do
+    it "responds to #dock with 1 argument" do
+      expect(subject).to respond_to(:dock).with(1).argument
+    end
+
+    it "able to dock bike if empty" do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq([bike])
+    end
+
+    it "raises an error when full" do
+      20.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error 'Docking station full'
+    end
   end
 
-  it "expects a bike to be working" do
-    expect(subject.release_bike.working?).to eq true
-    # expect(Bike.new).to be_working
-  end
+  describe '#bike attribute' do
+    it "responds to bike method" do
+      expect(subject).to respond_to(:bikes)
+    end
 
-  it "responds to #dock with 1 argument" do
-    expect(subject).to respond_to(:dock).with(1).argument
+    it "returns docked bikes" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bikes).to eq([bike])
+    end
   end
-
-  it "docks something" do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
-  end
-
-  it "responds to bike method" do
-    expect(subject).to respond_to(:bike)
-  end
-
-  it "returns docked bikes" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
-
 end
