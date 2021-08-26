b5b6bfdc1dff2621a600e1745adbbd15f305c203
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-08-26 16:17:13 +0100

    chapter 10 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,16 @@ require "docking_station"
 describe DockingStation do
   it "DockingStation object to respond to .release_bike" do
     expect(subject).to respond_to(:release_bike)
+    p subject
     # it { is_expected.to respond_to :release_bike }
+  end
+
+  it "get a bike from .release_bike" do
+    expect(subject.release_bike).to be_a Bike
+  end
+
+  it "expects a bike to be working" do
+    expect(subject.release_bike.working?).to eq true
+    # expect(Bike.new).to be_working
   end
 end
