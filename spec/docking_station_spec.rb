38228caa00becfbddb4bd8af06e11b39f295f36f
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-08-26 17:44:42 +0100

    chapter 11 completed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -15,4 +15,24 @@ describe DockingStation do
     expect(subject.release_bike.working?).to eq true
     # expect(Bike.new).to be_working
   end
+
+  it "responds to #dock with 1 argument" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it "responds to bike method" do
+    expect(subject).to respond_to(:bike)
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
